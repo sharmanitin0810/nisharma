43f6d65fee45e9a98b83dcfe1ded4c592f67d785
Nitin Sharma <sharmanitin0810@gmail.com>
2020-11-29 02:37:52 +0530

    login remote m/c and run command and write into out text file

--- PYTHON/py_reference/remotelogin.py
+++ PYTHON/py_reference/remotelogin.py
@@ -1,10 +1,17 @@
 import paramiko
 import os
 
+process = os.getpid()
+
+print("PID is :",process)
 host=input("Please Enter the IP Address of Host")
 
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.connect(host, username='nisharma_hsc', password='nitin@0810',port=22)
-stdin, stdout, stderr = client.exec_command('sudo ls -lrt', get_pty=True)
-print(stdout.read())
+client.connect(host, username='nisharma_hsc', password='oran@123',port=22)
+with open ('remote_ouput.txt','w') as file:
+	stdin, stdout, stderr = client.exec_command('iperf -s -p 16176 -i 2 -t 10', get_pty=True)
+	stdout_final = stdout.read().decode('ascii')
+	print(stdout_final)
+	file.write("\t Server Output \n")
+	file.write(str(stdout_final))
