62aff3b33f0a1a910e1a7b2cb405200f87ea4f9a
Nitin Sharma <sharmanitin0810@gmail.com>
2021-03-30 20:13:47 +0530

    Removed hardcord paths and added logic to chk connecitivty of nodes first and then proceed

--- PYTHON/ow_pyscripts/AUTO_COMMAND/push_cmd.py
+++ PYTHON/ow_pyscripts/AUTO_COMMAND/push_cmd.py
@@ -6,7 +6,7 @@
 # REVISION HISTORY:
 #          DATE:           AUTHOR:         CHANGE:
 #          12-02-20        Nitin Sharma    Initial version
-#          14-02-20        Nitin Sharma    Final version
+#          30-03-21        Nitin Sharma    Final version
 #
 # Copyright 2019 Hughes Network Systems Inc
 #-----------------------------------------------------------------------------------------
@@ -55,29 +55,53 @@ def create_log_dir():
            if e.errno == errno.EEXIST:
               print("Log Directory already Exist - Not Creating Again")
 
+def chk_node_status(node_ip):
+	try:	
+		print('Going to check the IP connectivity of node :' + str(node_ip))
+		#node_rsp = os.system("ping -i 1 -c 3" + node_ip)
+		node_rsp = subprocess.check_output(["ping", "-c", "3", node_ip])
+		return True
 
-def working():
-        f=open("/home/msat/LOGS/prd_gn/ip-list.txt")
-        g=open("/home/msat/LOGS/prd_gn/cmd-list.txt")
-        cmd=g.read()
+	except subprocess.CalledProcessError:
+		return False
+
+def push_cmds():
+	current_dir = os.getcwd()
+        target_nodes=open("ip-list.txt")
+        user_cmd=open("cmd-list.txt")
+        cmd=user_cmd.read()
         #print(cmd)
         USERNAME='msat'
         PASSWORD='oneweb123'
-        #rcmd='touch /tmp/a; touch /tmp/b; ls -lrt>/tmp/abb.log'
-        for line in f:
+        for line in target_nodes:
                    print('Accessing Host :' + str(line))
-                   logger.info("Entering for loop")
-                   ssh = paramiko.SSHClient()
-                   ssh.set_missing_host_key_policy( paramiko.AutoAddPolicy() )
-                   ssh.load_host_keys(os.path.expanduser(os.path.join("~", ".ssh", "known_hosts")))
-                   try:
-                      ssh.connect(line, username=USERNAME, password=PASSWORD, timeout = 10)
-		      stdin, stdout, stderr = ssh.exec_command(cmd)
-		      print('Command Executed : '+ str(cmd))
-                      logger.info("SSH and Command run successfully")
-                      ssh.close()
-                   except Exception, e :
-                      logger.critical('Not reachable',e)
+	           node_stat = chk_node_status(line)
+		   #print("NOde stats :",node_stat)
+	           if node_stat == True:
+			print("[SUCCESS] GN component :" +str(line)+ " " + "is reachable")
+			print("Executing commands on GN component ..Please wait..")
+                   	logger.info("Entering for loop")
+	                ssh = paramiko.SSHClient()
+        	        ssh.set_missing_host_key_policy( paramiko.AutoAddPolicy() )
+                	ssh.load_host_keys(os.path.expanduser(os.path.join("~", ".ssh", "known_hosts")))
+	                try:
+        	           ssh.connect(line, username=USERNAME, password=PASSWORD, timeout = 10)
+			   stdin, stdout, stderr = ssh.exec_command(cmd)
+			   print(" ********* Command Output ************ \n ")
+			   for line in stdout.read().splitlines():
+			      print(line)
+
+		     	   print(' *********** Command(s) Executed *************** : \n'+ str(cmd))
+	                   logger.info("SSH and Commands executed successfully")
+        	           ssh.close()
+
+                	except Exception as e:
+	                      logger.critical('Not reachable',e)
+		   else:
+			print("[FAILED] GN component:" +str(line)+ " "+ "is not reachable")
+
+	target_nodes.close()
+	user_cmd.close()
 
 if __name__=="__main__":
 
@@ -97,4 +121,4 @@ if __name__=="__main__":
         logger.info("Log directory is successfully created")
         os.system("clear")
 
-        working()
+        push_cmds()
